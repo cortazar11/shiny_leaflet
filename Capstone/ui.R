5e9abb8f0359b3cf150552be4e59ab4ad8faba2f
Miguel Martinez <miguel.martinez11@gmail.com>
2023-05-03 12:26:29 +0200

    Update ui.R

--- Capstone/ui.R
+++ Capstone/ui.R
@@ -1,22 +1,22 @@
 # Load required libraries
 require(leaflet)
-cities <- c("London","Paris","New York","Seoul","Suzhou")
+cities <- c("All","London","Paris","New York","Seoul","Suzhou")
 
 # Create a RShiny UI
 shinyUI(
   fluidPage(padding=5,
-  titlePanel("Bike-sharing demand prediction app"), 
-  # Create a side-bar layout
-  sidebarLayout(
-    # Create a main panel to show cities on a leaflet map
-    mainPanel(
-      # leaflet output with id = 'city_bike_map', height = 1000
-      leafletOutput("city_bike_map", height = 1000)
-    ),
-    # Create a side bar to show detailed plots for a city
-    sidebarPanel(
-      # select drop down list to select city
-      selectInput("city", "Choose a color:", choices = cities)
-      #textOutput("result")
-    ))
-))
+            titlePanel("Bike-sharing demand prediction app"), 
+            # Create a side-bar layout
+            sidebarLayout(
+              # Create a main panel to show cities on a leaflet map
+              mainPanel(
+                # leaflet output with id = 'city_bike_map', height = 1000
+                leafletOutput("city_bike_map", height = 1000)
+              ),
+              # Create a side bar to show detailed plots for a city
+              sidebarPanel(
+                # select drop down list to select city
+                selectInput("city_dropdown", "Choose a color:", choices = cities)
+                #textOutput("result")
+              ))
+  ))
